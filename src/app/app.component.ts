cd77b19d90acfa0d79b8b6f397a2db73af1962b7
nmcapule <llss.s.sls.lll@gmail.com>
2017-08-02 06:20:23 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,12 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      if (platform.is('cordova')) {
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        statusBar.styleDefault();
+        splashScreen.hide();
+      }
     });
   }
 }
